ec69199de222e5f38a4c5bdf44baa537d7e3634a
Maninder Singh <manindersingh2400@gmail.com>
2024-06-07 11:44:37 +0530

    Add project files.

---
+++ SellBooksEcommerce/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SellBooksEcommerce.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
